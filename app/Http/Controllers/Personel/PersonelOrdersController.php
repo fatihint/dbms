ebe498047f593e6e1419b7a1a859dd08f992a4ae
Baran Ozoglu <abaranozoglu@gmail.com>
2017-12-22 05:14:22 +0300

    personel controller database baglantilari tamamlandi

--- app/Http/Controllers/Personel/PersonelOrdersController.php
+++ app/Http/Controllers/Personel/PersonelOrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Personel;
 
+use App\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,25 +10,38 @@ class PersonelOrdersController extends Controller
 {
     public function index()
     {
-        return view('personel.orders');
+        $orders = Order::all();
+
+        return view('personel.orders')->with(
+            ["orders" => $orders]
+        );
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Product  $product
+     * @param  \App\Product  $order
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        return view('personel.orders')->with(
-            ['id' => $id]
-        );
+        $order = Order::find($id);
+
+        if ($order) {
+            return view('personel.orders')->with(
+                ["order" => $order]
+            );
+        } else {
+            return redirect('/personel/orders');
+        }
     }
 
     public function accept(Request $request)
     {
-
+        $order = Order::find($request->id);
+        $order->status_id = 3;
+        $order->updated_at = date('Y-m-d H:i:s');
+        $order->save();
         return view('personel.accept')->with(
             ['id' => $request->id]
         );
@@ -36,7 +50,7 @@ class PersonelOrdersController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Product  $product
+     * @param  \App\Product  $order
      * @return \Illuminate\Http\Response
      */
     public function destroy(Order $order)
